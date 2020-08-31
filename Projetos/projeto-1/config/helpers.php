1debcff465e2b4addbcc64207a3a09512317f8b7
andre <andremarcinichen@hotmail.com>
2020-08-31 18:00:54 -0300

    aulas

---
+++ Projetos/projeto-1/config/helpers.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+*
+* Arquivo onde são definidos os helpers
+*
+* @author Code Universe
+*
+**/
+
+define('LOCAL_URL', 'Andre_ramos/Aulas-code-universe/Projetos/projeto-1');
+
+	
+// if($env == "dev"){
+// 	define('LOCAL_URL', '/pasta_do_projeto');
+// }
+// elseif($env == "prod"){
+// 	define('LOCAL_URL', 'https://www.google.com.br/');
+// }
+
+return array(
+	'URLHelper' 	=> new URLHelper(),
+);
---
+++ Projetos/projeto-1/views/site/shared/layout.php
@@ -0,0 +1,24 @@
+<?php
+	$url = $this->helpers['URLHelper']->getURL();
+	$location = $this->helpers['URLHelper']->getLocation();
+	// var_dump($url);
+	// echo "<br>";
+	// var_dump($location);
+	// die;
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name= "keywords" content="HTML, CSS, JS, PHP">
+		<meta name= "description" content="descrição do site">
+		<meta name= "author" content="code universe">
+		<meta name= "viewport" content="width=device-width, initial-scale=1.0">
+		<meta name= "robots" content="index, follow">
+		<title></title>
+	</head>
+	<body>
+
+	</body>
+</html>
