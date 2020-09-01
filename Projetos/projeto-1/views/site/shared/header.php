c52330472e4bba1b0dc837a44d7b0221cb63dbf1
andre <andremarcinichen@hotmail.com>
2020-09-01 17:58:38 -0300

    aulas

---
+++ Projetos/projeto-1/views/site/shared/header.php
@@ -0,0 +1,20 @@
+
+<!-- cabeçalho -->
+
+<div class="container">
+<!-- arruma as margens -->
+	<div class="float-left">
+		<a href="#" class="text-light">logo</a>	
+	</div>
+
+	<!-- hamburguer -->
+
+	<!-- links -->
+	<div class="float-right">
+			<a href="#" class="text-light d-block">	home</a>
+			<a href="#" class="text-light d-block">	quem somos</a>
+			<a href="#" class="text-light d-block">	contato</a>
+
+	</div>
+
+</div>
--- Projetos/projeto-1/views/site/shared/layout.php
+++ Projetos/projeto-1/views/site/shared/layout.php
@@ -7,6 +7,8 @@
 	// die;
 ?>
 
+<!-- AQUI TU VAI CHARMAR TODOS OS ARQUIVOS DE CSS PARA O SITE -->
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -15,10 +17,36 @@
 		<meta name= "description" content="descrição do site">
 		<meta name= "author" content="code universe">
 		<meta name= "viewport" content="width=device-width, initial-scale=1.0">
-		<meta name= "robots" content="index, follow">
-		<title></title>
+		<meta name= "robots" content="index, follow"> 
+		<!-- palavras chaves no primeiro meta colocar VARIAS TAGS -->
+		<!-- descrição no google -->
+		<!-- view port para celular -->
+		<!-- index flow sao subpginas para aparecer no google pode usar noindex ou nofollow ou nocontatos etc.... -->
+		<title>projetos</title>
+		<link rel="stylesheet" type="text/css" href="<?php echo $url; ?>/assets/libs/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo $url; ?>/assets/css/site/layout.css">
+
 	</head>
 	<body>
+		<header>
+			<?php require 'header.php'; ?>
+		</header>
 
+		<main>
+			<?php require $file; ?>
+		</main>
+
+		<footer>
+			<?php require 'footer.php'; ?>
+		</footer>
+
+		<script type="text/javascript">
+			var URL = "<?php echo $url; ?>";
+			var helpers = {};
+		</script>
+
+		<script type="text/javascript" src="<?php echo $url; ?>/assets/libs/jquery/jquery-3.4.1.min.js"></script>
+		<script type="text/javascript" src="<?php echo $url; ?>/assets/libs/popper/pooper.min.js"></script>
+		<script type="text/javascript" src="<?php echo $url; ?>/assets/libs/bootstrap/js/bootstrap.min.js"></script>
 	</body>
 </html>
