5dcb160e441846225172650ea76684052a68cf2b
andre <andremarcinichen@hotmail.com>
2020-10-07 16:31:56 -0300

    aulas

--- Node/aula 3/connection.js
+++ Node/aula 3/connection.js
@@ -14,3 +14,34 @@ sequelize.authenticate().then(function(){
 }).catch(function(erro){
 	console.log("Não foi possível conectar: " + erro)
 })
+
+const usuarios = sequelize.define('usuarios', {
+	firstname: {
+		type: Sequelize.STRING
+	},
+	lastname: {
+		type: Sequelize.STRING
+	}
+})
+
+// usuarios.sync({force: true})
+
+// usuarios.create({
+// 	firstname: "Mario",
+// 	lastname: "Bros"
+// })
+
+
+// sequelize.query('SELECT * FROM usuarios',
+//   {type: sequelize.QueryTypes.SELECT }
+// ).then(function(usuarios) {
+//   console.log(usuarios)
+// })
+
+// sequelize.query('UPDATE usuarios SET firstname = "Super Mario" WHERE id = 1',
+//   {type: sequelize.QueryTypes.UPDATE}
+// )
+
+sequelize.query('DELETE from usuarios WHERE id = 1',
+  {type: sequelize.QueryTypes.DELETE}
+)
