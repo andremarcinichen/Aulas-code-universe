6c4d985da265b267ce2a2e44ebe45a2795db568a
andre <andremarcinichen@hotmail.com>
2020-10-14 16:05:49 -0300

    aulas

---
+++ Node/tarefa/models/usuarios.js
@@ -0,0 +1,13 @@
+const db = require("./db")
+
+const usuarios = db.sequelize.define("usuarios", {
+	firstname: {
+		type: db.Sequelize.STRING
+	},
+	lastname: {
+		type: db.Sequelize.STRING
+	}
+})
+
+module.exports = usuarios
+// usuarios.sync({force: true})
