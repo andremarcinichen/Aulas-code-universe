c6aaefb387b675cb0012daa22d2267b2a1f359f9
andre <andremarcinichen@hotmail.com>
2020-10-16 18:01:44 -0300

    aulas

---
+++ C#/tarefa2/tarefepdf/tarefepdf/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tarefepdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite o seu sexo");
+            string sexo = Console.ReadLine();
+            Console.WriteLine("Digite o seu endereço ");
+            string endereco = Console.ReadLine();
+            Console.WriteLine("Digite a sua idade");
+            string idade = Console.ReadLine();
+            Console.WriteLine("Digite a sua cep");
+            string seucep = Console.ReadLine();
+        Console.WriteLine("Seu nome é " + nome + " o seu sexo é: " + sexo + " o seu endereço é: " + endereco + " sua idade é: " + idade + " e seu cep é: " + seucep);
+        
+        }
+    }
+}
